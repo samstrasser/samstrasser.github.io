516854f9b23283ff395ca37d28f83598396fd84f
Sam Strasser <sam.strasser@gmail.com>
2015-07-20 18:45:58 -0700

    Fix encoding issues

--- proj/asana-json/build/download_link.js
+++ proj/asana-json/build/download_link.js
@@ -3,7 +3,7 @@ var DownloadLink = React.createClass({displayName: "DownloadLink",
     var rows = ['Funnel Step,Task,Tags,'];
     sections.forEach(function(section) {
       section.tasks.forEach(function(task) {
-        var row = [section.name, task.name].map(function(item) { 
+        var row = [section.name, task.name].map(function(item) {
           return '"' + item + '"';
         }).concat(task.tags.map(function(t) { return t.name; }).join(';'));
         rows.push(row.join(','));
@@ -11,12 +11,19 @@ var DownloadLink = React.createClass({displayName: "DownloadLink",
     });
     return rows;
   },
-  
+
+  // https://developer.mozilla.org/en-US/docs/Web/API/WindowBase64/Base64_encoding_and_decoding#The_.22Unicode_Problem.22
+  b64EncodeUnicode: function(str) {
+    return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function(match, p1) {
+        return String.fromCharCode('0x' + p1);
+    }));
+  },
+
   render: function() {
     var rows = this.generateRows(this.props.sections);
-    var encoded = btoa(rows.join("\n"));
+    var encoded = this.b64EncodeUnicode(rows.join("\n"));
     var href = "data:text/csv;base64," + encoded;
-    
+
     return (
       React.createElement("a", {className: "btn btn-default download", 
         download: "tasks.csv", 
--- proj/asana-json/src/download_link.jsx
+++ proj/asana-json/src/download_link.jsx
@@ -3,7 +3,7 @@ var DownloadLink = React.createClass({
     var rows = ['Funnel Step,Task,Tags,'];
     sections.forEach(function(section) {
       section.tasks.forEach(function(task) {
-        var row = [section.name, task.name].map(function(item) { 
+        var row = [section.name, task.name].map(function(item) {
           return '"' + item + '"';
         }).concat(task.tags.map(function(t) { return t.name; }).join(';'));
         rows.push(row.join(','));
@@ -11,16 +11,23 @@ var DownloadLink = React.createClass({
     });
     return rows;
   },
-  
+
+  // https://developer.mozilla.org/en-US/docs/Web/API/WindowBase64/Base64_encoding_and_decoding#The_.22Unicode_Problem.22
+  b64EncodeUnicode: function(str) {
+    return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function(match, p1) {
+        return String.fromCharCode('0x' + p1);
+    }));
+  },
+
   render: function() {
     var rows = this.generateRows(this.props.sections);
-    var encoded = btoa(rows.join("\n"));
+    var encoded = this.b64EncodeUnicode(rows.join("\n"));
     var href = "data:text/csv;base64," + encoded;
-    
+
     return (
-      <a className="btn btn-default download" 
-        download="tasks.csv" 
-        href={href} 
+      <a className="btn btn-default download"
+        download="tasks.csv"
+        href={href}
         title="Download as CSV"
         >
       <img src="img/excel.png" /> Download
