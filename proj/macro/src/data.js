c114eb346cdf4fbb3bace9aa10a2699b98982750
Sam Strasser <sam.strasser@mybrightwheel.com>
2021-07-16 15:13:45 -0400

    Remove proj folder

--- proj/macro/src/data.js
+++
@@ -1,9 +0,0 @@
-var globalData = [
-  'https://www.newyorker.com/magazine/2013/07/29/slow-ideas',
-  'https://www.theatlantic.com/magazine/archive/2014/06/the-case-for-reparations/361631/',
-  'https://blogs.edweek.org/edweek/rick_hess_straight_up/2018/02/a_confession_and_a_question_on_personalized_learning.html',
-
-  'https://www.theatlantic.com/magazine/archive/2018/06/the-birth-of-a-new-american-aristocracy/559130/',
-  'https://www.africa.upenn.edu/Articles_Gen/Letter_Birmingham.html'
-  'https://en.wikisource.org/wiki/The_Gospel_of_Wealth'
-]
